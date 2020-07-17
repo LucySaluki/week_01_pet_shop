9b2ba38660449f76e2558b28550f6f7e312c2f67
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-17 13:49:11 +0100

    Test 7 added - get stock count

--- pet_shop_start_point/src/pet_shop.py
+++ pet_shop_start_point/src/pet_shop.py
@@ -18,3 +18,7 @@ def get_pets_sold(pet_shop):
 def increase_pets_sold(pet_shop, number_of_pets):
     pet_shop["admin"]["pets_sold"]=pet_shop["admin"]["pets_sold"] + number_of_pets
     return pet_shop["admin"]["pets_sold"]
+
+# Test 7 - get stock count
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
