2d02b15b9017a67b4c4f469e6f368443711b306d
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-17 13:36:14 +0100

    Test 5 added - check the number of pets sold

--- pet_shop_start_point/src/pet_shop.py
+++ pet_shop_start_point/src/pet_shop.py
@@ -2,11 +2,16 @@
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
-# Test 1 - get total cash from pet shop 
+# Test 2 - get total cash from pet shop 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-# Test 2 - add or remove cash and get the total again
+# Test 3 and 4 - add or remove cash and get the total again
 def add_or_remove_cash(pet_shop,amount):
     pet_shop["admin"]["total_cash"]=pet_shop["admin"]["total_cash"]+ amount
-    
+
+# Test 5 - check the number of pets sold
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+# Test 6 - increase the number of pets_sold
